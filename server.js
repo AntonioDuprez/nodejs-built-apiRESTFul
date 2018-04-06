885370c9de7900c3c6f3ba5ab7a4509d919dff5f
Antonio Duprez <antonioduprezher@gmail.com>
2018-04-06 14:41:38 +0200

    Installing some important packages and create the information that we're going to use to work

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const http = require('http');
 
 const hostname = 'localhost';
@@ -10,5 +12,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server is running at htttp://${hostname}:${port}`);
+    console.log(`Server is running at http://${hostname}:${port}`);
 });
