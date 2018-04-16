cd12aff6469bd153887d187b352ab1c4696730ea
Antonio Duprez <antonioduprezher@gmail.com>
2018-04-16 11:38:02 +0200

    GET, POST, PUT, DELETE ready, and README modify to explain this utilities

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ We connected with the server at port 3001 (localhost) and we made a simple Hello
 ## Part 2 - Installing Packages and Create the information
 
 We installing some importants packages. For example, express, body-parser or cors that we will use 
+
+## Part 3 - GET Request
+
+We show all contacts and if there are not contacts we display a error message. Also we filter contacts by id.
+
+## Part 4 - POST, PUT, DELETE Request
+
+When we use POST we received a contact by request and we translate his body and push them into the contacts array. We don't modify the json file.
+
+When we use PUT we modify the contact information filtring by id and we overwrite his information in array contacts.
+
+When we use DELETE we delete the contact from his array (never in json file) and we display a basic information message.
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ let contacts = require('./data');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// Muestra todos los contactos existentes en nuestro archivo json
 app.get('/api/contacts', (req, res) => {
     if(!contacts){
         res.status(404).json({message: 'No contacts found'});
@@ -16,6 +17,7 @@ app.get('/api/contacts', (req, res) => {
     res.json(contacts);
 });
 
+// Filtra los contactos mediante el id introducido por GET
 app.get('/api/contacts/:id', (req, res) => {
     const reqId = req.params.id;
 
@@ -29,6 +31,61 @@ app.get('/api/contacts/:id', (req, res) => {
     res.json(contact);
 });
 
+// Añade un contacto según los parámetros enviados por POST al final de nuestro json 
+// (no se guarda actualmente, solo se almacena hasta que se cierra el servidor, petición o navegador)
+app.post('/api/contacts', (req, res) => {
+    const contact = {
+        id: contacts.length + 1,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        website: req.body.website
+    }
+
+    contacts.push(contact);
+
+    res.json(contact);
+});
+
+// Permite modificar los datos de un contacto filtrado por el id
+// Primero filtra por el id y después sería como un post ?req.datos
+app.put('/api/contacts/:id', (req, res) =>{
+    const reqId = req.params.id;
+
+    let contact = contacts.filter(contact => {
+        return contact.id == reqId;
+    })[0];
+
+    const index = contacts.indexOf(contact);
+    const keys = Object.keys(req.body);
+
+    keys.forEach(key => {
+        contact[key] = req.body[key];
+    });
+
+    contacts[index] = contact;
+
+    res.json(contacts[index]);
+});
+
+// Elimina un contacto específico filtrado por Id.
+app.delete('/api/contacts/:id', (req, res) => {
+    const reqId = req.params.id;
+
+    let contact = contacts.filter(contact => {
+        return contact.id = reqId;
+    })[0];
+
+    const index = contacts.indexOf(contact);
+
+    // Elimina los elementos existentes pudiendo agregar, nuevos:
+    // Parametro 1: la posición a eliminar
+    // Parametro 2: cuantas posiciones a partir de esa quieres eliminar
+    // Parametro 3: (opcional) Porque quieres sustituir esas posiciones
+    contacts.splice(index, 1); // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/splice
+
+    res.json({ message: `User ${reqId} delete.`});
+});
 // GET, POST, PUT, DELETE, PATCH
 
 const hostname = 'localhost';
