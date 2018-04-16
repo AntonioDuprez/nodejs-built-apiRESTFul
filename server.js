f12415cf7244e72240c60d18ad8f3b71c814db03
Antonio Duprez <antonioduprezher@gmail.com>
2018-04-16 10:35:37 +0200

    Server with express module ready and we used the postman app to watched the server response information

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 'use strict';
 
-const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+let contacts = require('./data');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/api/contacts', (req, res) => {
+    if(!contacts){
+        res.status(404).json({message: 'No contacts found'});
+    }
+    res.json(contacts);
+});
+
+app.get('/api/contacts/:id', (req, res) => {
+    const reqId = req.params.id;
+
+    let contact = contacts.filter(contact => {
+        return contact.id == reqId;
+    });
+
+    if(contact == ""){
+        res.status(404).json({ message: 'No contact found' });
+    }
+    res.json(contact);
+});
+
+// GET, POST, PUT, DELETE, PATCH
 
 const hostname = 'localhost';
 const port = 3001;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end('Hello world!\n');
-});
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server is running at http://${hostname}:${port}`);
 });
